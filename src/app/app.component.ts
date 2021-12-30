43e8be357c33ed3af13b34ea1bcb188ab5c0fd74
wajeehmisbahkhan <wajeehmisbahkhan@yahoo.com>
2021-12-30 23:59:16 +0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, VERSION } from '@angular/core';
+import { name, datatype, helpers, random } from 'faker';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+  users: User[] = [];
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      const { name, username, email } = helpers.userCard();
+      this.users.push({
+        id: i,
+        name,
+        username,
+        email,
+        active: datatype.boolean(),
+        group: random.arrayElement<'Admin' | 'User'>(['Admin', 'User']),
+        lastLogin: datatype.datetime(),
+      });
+    }
+  }
+}
+
+interface User {
+  id: number;
+  username: string;
+  lastLogin: Date;
+  group: 'User' | 'Admin';
+  name: string;
+  active: boolean;
+  email: string;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TableModule } from 'primeng/table';
+import { AvatarModule } from 'primeng/avatar';
+import { CheckboxModule } from 'primeng/checkbox';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    TableModule,
+    AvatarModule,
+    CheckboxModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
